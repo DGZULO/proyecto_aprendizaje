8ce8683178fb07eb9822962cf8007e7b6fb3b7e3
alphilla <alphahuevon@gmail.com>
2022-03-16 16:50:50 +0100

    Comienzo,suma y multiplicación

--- git.py
+++ git.py
@@ -2,14 +2,13 @@
 
 ### Código juguete para aprender Git & Github
 
-##Fase 1
+##Fase 1 Hola mundo
 
 variable="hola mundo"
 
 print(variable)
 
-## Fase 2
-
+## Fase 2 Agregando suma
 
 
 def suma(n1,n2):
@@ -24,3 +23,21 @@ v1=int(input("introducir primer numero: "))
 v2=int(input("introducir segundo numero: "))
     
 suma(v1,v2)
+
+## Fase 3 Agregando suma
+
+
+def multiplicación(n1,n2):
+    
+    resultado=n1*n2
+    
+    return resultado
+
+
+v1=int(input("introducir primer numero: "))
+
+v2=int(input("introducir segundo numero: "))
+    
+multiplicación(v1,v2)
+
+
