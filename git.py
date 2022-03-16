ff4971acc55b44a899f6c48302f7248f76f4cd17
alphilla <alphahuevon@gmail.com>
2022-03-16 16:46:08 +0100

    Agregando Suma

--- git.py
+++ git.py
@@ -9,3 +9,18 @@ variable="hola mundo"
 print(variable)
 
 ## Fase 2
+
+
+
+def suma(n1,n2):
+    
+    resultado=n1+n2
+    
+    return resultado
+
+
+v1=int(input("introducir primer numero: "))
+
+v2=int(input("introducir segundo numero: "))
+    
+suma(v1,v2)
